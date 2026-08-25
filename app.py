66b7db00143e86add3550d8fc369c2d663104c76
wahidwahabsial786-byte <wahidwahabsial786@gmail.com>
2026-08-26 02:29:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def webhook():
 
         print("→ Gemini ko bhej raha hoon...")
         response = client.models.generate_content(
-            model="gemini-2.0-flash",
+            model="gemini-2.5-flash",
             contents=user_message
         )
 
