898262a7d745e5579bcbf4c747e68214391291de
wahidwahabsial786-byte <wahidwahabsial786@gmail.com>
2026-08-27 14:53:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def webhook():
         print("→ Groq ko bhej raha hoon...", flush=True)
 
         response = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
+            model="openai/gpt-oss-20b"
             messages=[
                 {
                     "role": "system",
